9af400727d8a1dba80e1f953a333317dbe74f96a
hardgosu <deven1@naver.com>
2018-09-11 22:21:47 +0900

    Drill- 03 완성
    2013182018 박민욱
    https://www.youtube.com/watch?v=ymT3i6FwAeI&feature=youtu.be

---
+++ Drills-03/drills-03.py
@@ -0,0 +1,89 @@
+from pico2d import *
+import math
+
+
+
+
+open_canvas()
+
+
+os.chdir('C:\\temp1\\lab01')
+
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+#stack = 0
+
+#test = x+(y * y)
+
+#print(math.sqrt(test))
+#print(abs(3))
+    
+
+
+while(True):
+
+    x = 0
+    y = 90
+
+
+
+    while ( x < 400):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+
+        character.draw_now(x, y)
+        x = x + 2
+        delay(0.01)
+
+
+    degree = 360
+    i = 0
+
+    while( i <= degree):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+
+        character.draw_now(x,y)
+        x = 400 -200 * math.sin(math.radians(i))
+        y = 285  -200* math.cos(math.radians(i))
+        i = i + 1
+        delay(0.01)
+        
+
+    while (x < 800):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+
+        character.draw_now(x, y)
+        x = x + 2
+        delay(0.01)
+
+
+
+    while (y < 600):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y = y + 2
+        delay(0.01)
+
+
+    while (x > 0):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+
+        character.draw_now(x, y)
+        x = x - 2
+        delay(0.01)
+
+    while (y > 90):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y = y - 2
+        delay(0.01)
+
+
+close_canvas()
