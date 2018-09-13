dc0c8dd5050c8ccd11f878d71b43b182502d5a8f
hardgosu <deven1@naver.com>
2018-09-13 18:51:04 +0900

    Drill 04 완성
    2013182018 박민욱
    https://www.youtube.com/watch?v=-a1sGY8T64U&feature=youtu.be
    전체 반복문 누락으로  추가함

--- Drills-04/Drill-04.py
+++ Drills-04/Drill-04.py
@@ -16,35 +16,35 @@ frontDirection = True
 backDirection = False
 
 animation = load_image('animation_sheet.png')
-row = 2
 
 
 
 
-row = 1
+while(True):
+    row = 1
 
-while (x < 800):
-    clear_canvas()
-    grass.draw(400, 30)
-    animation.clip_draw(frame * 100, 100 * row, 100, 100, x, 90)
-    frame = (frame + 1) % 8
-    x = x + 15
+    while (x < 800):
+        clear_canvas()
+        grass.draw(400, 30)
+        animation.clip_draw(frame * 100, 100 * row, 100, 100, x, 90)
+        frame = (frame + 1) % 8
+        x = x + 15
 
-    update_canvas()
-    delay(0.05)
-    get_events()
+        update_canvas()
+        delay(0.05)
+        get_events()
 
-row = 0
-while (x > 0 ):
-    clear_canvas()
-    grass.draw(400, 30)
-    animation.clip_draw(frame * 100, 100 * row, 100, 100, x, 90)
-    frame = (frame + 1) % 8
-    x = x - 15
+    row = 0
+    while (x > 0 ):
+        clear_canvas()
+        grass.draw(400, 30)
+        animation.clip_draw(frame * 100, 100 * row, 100, 100, x, 90)
+        frame = (frame + 1) % 8
+        x = x - 15
 
-    update_canvas()
-    delay(0.05)
-    get_events()
+        update_canvas()
+        delay(0.05)
+        get_events()
 
 
 close_canvas()
