ea248fd4b89e041ba2f2f4503b9c5525494a3a2f
hardgosu <deven1@naver.com>
2018-09-18 17:50:02 +0900

    2013182018 박민욱 Drill-05 commit 2
    Drill-05
    함수의 기본 뼈대를 만드는 법,
    pass구문으로 함수의 작성을 일단 보류하는법, 함수의 작명을 효과적으로 하는법을 배웠다

--- Drill-05/Drill-05.py
+++ Drill-05/Drill-05.py
@@ -5,6 +5,22 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-# fill here
-    
+#오늘은 진도 안나가고 이대현교수님의 특강 18 09 18
+
+def make_rectangle():
+    open_canvas()
+    pass
+
+def make_circle():
+    pass
+
+
+
+while True:
+    make_rectangle()
+    make_circle()
+    #throw_rectangle()
+    #throw_circle()
+
+
 close_canvas()
