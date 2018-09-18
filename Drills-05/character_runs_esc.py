d62d6a21592ff4360c3c743a1496be57db9b835e
hardgosu <deven1@naver.com>
2018-09-18 15:42:33 +0900

    2013182018 Drill-05 Commit1
    지정 좌표로 한칸씩 이동하는
    GotoSomeCoordinate와
    그걸 지정 좌표에 도달할때까지
    무한 루프하는 GotoSomeCordinateUntilEnd 작성.
    delay = 0.05에서 0.01로 바꾸고 빠르게 진행.
    현재 203,535를 인자로 넘겨받는
    GotoSomeCordinateUntilEnd는 정상 작동

---
+++ Drills-05/character_runs_esc.py
@@ -0,0 +1,109 @@
+from pico2d import *
+
+
+def handle_events():
+    global running
+    global dir
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                 dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
+
+
+def GotoSomeCoordinate(x,y,curX,curY):
+    global dirX
+    global dirY
+    if curX < x:
+        dirX += 1
+    elif curX > x:
+        dirX -= 1
+    if curY < y:
+        dirY += 1
+    elif curY >y:
+        dirY -= 1
+
+
+def GotoSomeCordinateUntilEnd(newX,newY):
+
+        global dirX
+        global dirY
+        global frame
+        global row
+        global grass
+        global character
+        global running
+        global x
+        global y
+
+
+        while running:
+            dirX,dirY = 0,0
+
+            clear_canvas()
+            grass.draw(400, 30)
+            GotoSomeCoordinate(newX, newY, x, y)
+            # handle_events()
+            frame = (frame + 1) % 8
+            x += dirX
+            y += dirY
+            if dirX >= 0:
+                row = 1
+            elif dirX < 0:
+                row = 0
+
+            character.clip_draw(frame * 100, 100 * row, 100, 100, x, y)
+            delay(0.01)
+            update_canvas()
+            if dirX == 0 and dirY == 0:
+                break
+
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+dirX = 0
+dirY = 0
+running = True
+x = 800 // 2
+frame = 0
+dir = 0
+y = 90
+row = 0
+
+GotoSomeCordinateUntilEnd(203,535)
+
+while running:
+    dirX = 0
+    dirY = 0
+    clear_canvas()
+    grass.draw(400, 30)
+    GotoSomeCoordinate(203,535,x,y)
+    #handle_events()
+    frame = (frame + 1) % 8
+    x += dirX
+    y += dirY
+    if dirX >= 0:
+        row = 1
+    elif dirX < 0:
+        row = 0
+
+    character.clip_draw(frame * 100, 100 * row, 100, 100, x, y)
+    delay(0.05)
+    update_canvas()
+    if dirX == 0 and dirY == 0:
+        break
+
+close_canvas()
---
+++ Drills-05/move_character_with_mouse.py
@@ -0,0 +1,43 @@
+from pico2d import *
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+
+def handle_events():
+    global running
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+
+# fill here
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+frame = 0
+dir = 0
+hide_cursor()
+
+while running:
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    x += dir * 5
+    delay(0.02)
+    handle_events()
+
+close_canvas()
+
+
+
+
