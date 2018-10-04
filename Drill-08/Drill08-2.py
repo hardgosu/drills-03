acded4f18ac1d8f659e4e30c996b66eff0075e32
hardgosu <deven1@naver.com>
2018-10-04 18:42:45 +0900

    2013182018 박민욱 drill08-2 commit :  1
    drill 08-2 시작

--- Drill-08/Drill08-2.py
+++ Drill-08/Drill08-2.py
@@ -1,11 +1,12 @@
 from pico2d import *
 import random
+
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
+
 class Boy():
-
-
     pass
+
 
 def handle_events():
     global running
@@ -19,19 +20,20 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            SetDestination(event.x,KPU_HEIGHT  - 1 - event.y)
-            #print(event.y)
+            SetDestination(event.x, KPU_HEIGHT - 1 - event.y)
+            # print(event.y)
+
+
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
 
-def SetDestination(character,p1,p2):
-
-    global  x
-    global  y
-    global  frame
-    global  row
-    global  cursorY
-    global  cursorX
+def SetDestination(character, p1, p2):
+    global x
+    global y
+    global frame
+    global row
+    global cursorY
+    global cursorX
 
     for i in range(0, 100 + 1, 2):
         t = i / 100
@@ -40,15 +42,14 @@ def SetDestination(character,p1,p2):
         DrawCharacter(character)
         delay(0.02)
 
-def MoveCurve(character,p1,p2,p3,parameterLimitStart,parameterLimitEnd):
 
-
-    global  x
-    global  y
-    global  frame
-    global  row
-    global  cursorY
-    global  cursorX
+def MoveCurve(character, p1, p2, p3, parameterLimitStart, parameterLimitEnd):
+    global x
+    global y
+    global frame
+    global row
+    global cursorY
+    global cursorX
 
     for i in range(parameterLimitStart, parameterLimitEnd + 1, 2):
         t = i / 100
@@ -58,36 +59,38 @@ def MoveCurve(character,p1,p2,p3,parameterLimitStart,parameterLimitEnd):
         delay(0.02)
     pass
 
+
 def DrawBackground(background):
-    
     pass
+
+
 def RenderingPipeline():
-    
     pass
-#하나의 인스턴스를 참조해야한다.
+
+
+# 하나의 인스턴스를 참조해야한다.
 def DrawCharacter(character):
     global x
     global y
     global frame
     global row
-    global  points
+    global points
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * row, 100, 100, x-(33//2), y + (77/2 - 10))
+    character.clip_draw(frame * 100, 100 * row, 100, 100, x - (33 // 2), y + (77 / 2 - 10))
     cursor.draw(200, 200)
-    cursor.draw(cursorX,cursorY)
+    cursor.draw(cursorX, cursorY)
     update_canvas()
     frame = (frame + 1) % 8
     delay(0.02)
 
-
     pass
+
 
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 cursor = load_image('hand_arrow.png')
 noName = load_image('hand_arrow.png')
-
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
@@ -96,11 +99,10 @@ destinationX = 0
 destinationY = 0
 characterMoveFlag = False
 
-
 frame = 0
 dir = 0
 hide_cursor()
-cursorX,cursorY = KPU_WIDTH // 2, KPU_HEIGHT // 2
+cursorX, cursorY = KPU_WIDTH // 2, KPU_HEIGHT // 2
 
 row = 1
 
@@ -111,26 +113,14 @@ accumulatedY = 0
 
 size = 10
 
-points = [(random.randint(0,800),random.randint(0,600)) for i in range(size)]
-#draw_line_basic(points[0],points[1])
+points = [(random.randint(0, 800), random.randint(0, 600)) for i in range(size)]
+# draw_line_basic(points[0],points[1])
 # 잘라낸 캐릭터 크기 33 X 77
 # 전체 캐릭텅 미지 크기 42X92
 n = 0
 stamp = 0
 
-temp = [(200,200),(500,300)]
-while running:
-
-
-    for i in range(0,size):
-
-
-
-        MoveCurve(character,points[i % size],points[(i + 1) % size],points[(i + 2) % size],0,50)
-
-
-
-
+temp = [(200, 200), (500, 300)]
 
 
 
