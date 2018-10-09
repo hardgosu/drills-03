ba5f26aca74c72aebcdb681e7fda6c92349531cc
hardgosu <deven1@naver.com>
2018-10-09 18:38:08 +0900

    2013182018 박민욱 drill09 commit : 3
    축구공 객체 정의. 큰 공과 작은공의 구분은 생성자에서 정함.
    이벤트 처리함수 정함

--- Drill-09/Drill09.py
+++ Drill-09/Drill09.py
@@ -1,5 +1,34 @@
 from pico2d import *
 import random
+class SoccerBall:
+    def __init__(self):
+        self.x,self.y = random.randint(100,700),600
+        self.speedY = random.randint(5,15)
+        self.size = random.randint(0,1)
+        self.directionY = -1
+        self.positionLimitX = 0
+        self.positionLimitY = 0
+        if self.size == 0:
+            self.image = load_image('ball21x21.png')
+        else:
+            self.image = load_image('ball41x41.png')
+
+
+#포지션 제한? 사각 영역,원 영역... 그러나 지금은 y = n 같은 직선.
+    def update(self,limitX,limitY):
+        self.positionLimitY = limitY
+       #self.positionLimitX = limitX
+        for i in range(self.speedY):
+            self.y += 1 * self.directionY
+
+        for i in range(self.speedY):
+            if self.y < self.positionLimitY and self.positionLimitY != 0:
+                self.y -= 1 * self.directionY
+
+
+    def draw(self):
+        self.image.draw(self.x,self.y)
+
 
 class Grass:
     def __init__(self):
@@ -21,3 +50,19 @@ class Boy:
 
     def draw(self):
         self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
+
+
+
+# Game object class here
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+# initialization code
+open_canvas()
