ab9218d6325bede2539334236a2f63e243a69001
hardgosu <deven1@naver.com>
2018-10-11 21:52:30 +0900

    2013182018 박민욱 drill10-2 commit : 5
    게임 프레임워크에서 p키를 누르면
    고급 퍼즈 옵션이 실행되도록 한다

--- Drill-10-2/main_state.py
+++ Drill-10-2/main_state.py
@@ -75,7 +75,7 @@ def handle_events():
             game_framework.change_state(title_state)
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
             #game_framework.push_state(luxury_pause_state)
-            game_framework.push_state(pause_state)
+            game_framework.push_state(advanced_pause_state)
 def update():
     boy.update()
 
