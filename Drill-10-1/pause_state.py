40ae469277bd0462f9d0ca92219c9ccaedbd9138
hardgosu <deven1@naver.com>
2018-10-11 21:22:31 +0900

    2013182018 박민욱 drill10-1 commit : 3
    모듈 전체 정의.
    p키를 누르면 게임프레임워크는 pop을 한다.

--- Drill-10-1/pause_state.py
+++ Drill-10-1/pause_state.py
@@ -5,4 +5,51 @@ import main_state
 name = "PauseState"
 image = None
 
-print(os.chdir())
+#print(os.chdir())
+
+
+
+def enter():
+    global  image
+    image = load_image('pause.png')
+
+
+def exit():
+    global  image
+    del(image)
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type,event.key) ==(SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+                game_framework.pop_state()
+
+def draw():
+    clear_canvas()
+    image.draw(400,300)
+    update_canvas()
+
+
+
+
+
+
+
+
+def update():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
