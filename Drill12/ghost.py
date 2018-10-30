671cf5d4ba457b930517f356fafc51751bbdcdc9
hardgosu <deven1@naver.com>
2018-10-30 23:49:02 +0900

    2013182018 박민욱 Drill12 commit 7
    Drill12 최종
    고스트가 일어나는 애니메이션 추가...
    https://www.youtube.com/watch?v=MKslwJEFtfU&feature=youtu.be

--- Drill12/ghost.py
+++ Drill12/ghost.py
@@ -23,6 +23,11 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
 
+
+
+
+
+
 Degree = 0
 
 W = 4 * math.pi
@@ -43,41 +48,67 @@ class Ghost:
 
 
         self.timer = 0
-        self.opacify = 0.7
+        self.opacify = 0.5
 
         self.activate = False
 
         self.startPointX = 800
         self.startPointY = 400
 
+
+        self.riseKeyFrame = 120
+        self.currentFrame = 0
+        self.riseAnimation = False
+
     def start(self,boy):
         self.startPointX = boy.x
         self.startPointY = boy.y
-
-
+        self.currentFrame = 0
+        self.riseAnimation = True
 
 
         pass
 
     def update(self):
         global Degree
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        self.image.opacify(self.opacify)
+        if(self.riseAnimation == True):
+            self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+            self.image.opacify(self.opacify)
+            self.currentFrame += 1
+        else:
 
-        Degree = (Degree + math.degrees(W* game_framework.frame_time) ) % 720
 
-        self.opacify = (random.randint(0,10) % 11) / 10.0
+            self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+            self.image.opacify(self.opacify)
 
-        self.velocity = 3   * PIXEL_PER_METER
-        self.x = self.startPointX + self.velocity * math.cos(math.radians(Degree))
-        self.y = self.startPointY + self.velocity * math.sin(math.radians(Degree))
+            Degree = (Degree + math.degrees(W* game_framework.frame_time) ) % 720
+
+            self.opacify = (random.randint(0,10) % 11) / 10.0
+
+            self.velocity = 3 * PIXEL_PER_METER
+            self.x = self.startPointX + self.velocity * math.cos(math.radians(Degree))
+            self.y = self.startPointY + self.velocity * math.sin(math.radians(Degree))
 
 
 
         pass
 
     def draw(self):
-        if self.dir == 1:
-            self.image.clip_draw(int(self.frame) * 100, 300, 100, 100, self.x, self.y)
+
+        if self.riseAnimation == True:
+            if(self.currentFrame < 40):
+                self.image.clip_composite_draw(int(self.frame) * 100, 300, 100, 100, 3.141592 / 2, '', self.startPointX - 25, self.startPointY, 100, 100)
+            elif(self.currentFrame < 80):
+                self.image.clip_composite_draw(int(self.frame) * 100, 300, 100, 100, 3.141592 / 4, '',self.startPointX - 25, self.startPointY, 100, 100)
+            elif(self.currentFrame < 120):
+                self.image.clip_composite_draw(int(self.frame) * 100, 300, 100, 100, 0, '',self.startPointX - 25, self.startPointY, 100, 100)
+            elif(self.currentFrame >= 120):
+                self.riseAnimation = False
+
         else:
-            self.image.clip_draw(int(self.frame) * 100, 200, 100, 100, self.x, self.y)
+
+
+            if self.dir == 1:
+                self.image.clip_draw(int(self.frame) * 100, 300, 100, 100, self.x, self.y)
+            else:
+                self.image.clip_draw(int(self.frame) * 100, 200, 100, 100, self.x, self.y)
